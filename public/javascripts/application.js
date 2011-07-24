ac0988868094a98b48522b94bf77450f6c4e1726
Giovanni Cappellotto <potomak84@gmail.com>
2011-07-24 02:41:31 +0200

    group tomatoes by day; update tomatoes script
    add timer stages
    add hide and show UI utils

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,69 @@
-var pomodoroDuration = 0.1; // min
-var timer = pomodoroDuration*60;
+var timerInterval;
+
+function show(ids) {
+  ids.forEach(function(id) {
+    $("#"+id).show();
+  });
+}
+
+function hide(ids) {
+  ids.forEach(function(id) {
+    $("#"+id).hide();
+  });
+}
+
+function stateStart(timer) {
+  console.log("stateStart");
+  
+  $("#timer").html(timer);
+  
+  show(["timer", "squash"]);
+  hide(["start"]);
+}
+
+function stateCounting(timer) {
+  console.log("stateCounting");
+  
+  $("#timer").html(timer);
+}
+
+function stateStop() {
+  console.log("stateStop");
+  
+  hide(["timer", "squash"]);
+  show(["start"]);
+}
+
+function stateNewForm() {
+  console.log("stateNewForm");
+  
+  hide(["timer", "squash", "start"]);
+  show(["new_tomato_form"]);
+}
+
+function start(mins) {
+  console.log("start timer for " + mins + " mins");
+  
+  var timer = mins*60;
+  stateStart(timer);
+  
+  timerInterval = setInterval(function() {
+    timer--;
+    stateCounting(timer);
+    if(timer <= 0) {
+      clearInterval(timerInterval);
+      stateNewForm();
+    }
+  }, 1000);
+}
+
+function squash() {
+  console.log("squash tomato");
+  
+  clearInterval(timerInterval);
+  stateStop();
+}
 
 $(document).ready(function() {
-  $("#timer").html(pomodoroDuration*60);
   
-  var i = setInterval(function() {
-    $("#timer").html(--timer);
-  }, 1000);
-  
-  setTimeout(function() {
-    clearInterval(i);
-    $("#timer").html(0);
-  }, pomodoroDuration*60000);
 });
