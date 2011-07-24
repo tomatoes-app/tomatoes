3261229e96acee898826b2e4398e5ee1d37f8941
Giovanni Cappellotto <potomak84@gmail.com>
2011-07-24 02:40:24 +0200

    order by tomato creation date descending welcome#index list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     if current_user
       @tomato = current_user.tomatoes.build
-      @tomatoes = current_user.tomatoes.all
+      @tomatoes = current_user.tomatoes.order_by([[:created_at, :desc]])
     end
   end
 end
