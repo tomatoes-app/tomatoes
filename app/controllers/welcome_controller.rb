e6fa8ebf89f2bc64f4f87aa2df47305327cd2f69
Giovanni Cappellotto <potomak84@gmail.com>
2011-07-25 03:52:43 +0200

    update welcome#index style; add relative_day helper method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,8 @@ class WelcomeController < ApplicationController
     if current_user
       @tomato = current_user.tomatoes.build
       @tomatoes = current_user.tomatoes.order_by([[:created_at, :desc]]).group_by do |tomato|
-        tomato.created_at.strftime("%A, %B %e")
+        date = tomato.created_at
+        Time.mktime(date.year, date.month, date.day)
       end
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def relative_day(date)
+    case date.to_date
+    when Date.today
+      "Today"
+    when Date.yesterday
+      "Yesterday"
+    else
+      date.strftime("%A, %B %e")
+    end
+  end
 end
