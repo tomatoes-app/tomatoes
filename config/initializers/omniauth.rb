0bc985800c576ef0e1a57a3d6b0657919ff9735c
Giovanni Cappellotto <potomak84@gmail.com>
2011-07-29 16:48:35 +0200

    fix file not found exception for github auth configuration
    see http://travis-ci.org/potomak/tomatoes/builds/57552

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,12 @@
 if Rails.env.production?
   GITHUB = {'client_id' => ENV['client_id'], 'client_secret' => ENV['client_secret']}
 else
-  GITHUB = YAML.load_file("#{Rails.root}/config/github.yml")
+  begin
+    GITHUB = YAML.load_file("#{Rails.root}/config/github.yml")
+  rescue
+    # github.yml is not included in the repo
+    GITHUB = YAML.load_file("#{Rails.root}/config/github.example.yml")
+  end
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
