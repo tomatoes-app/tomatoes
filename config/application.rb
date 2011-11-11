7ec03fe22233b27b8f1a4dcd52196ea70d32b7ad
Giovanni Cappellotto <potomak84@gmail.com>
2011-11-11 17:41:42 +0100

    bump version number to 0.5.28

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "rails/test_unit/railtie"
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module TomatoesApp
-  VERSION = '0.5.27'
+  VERSION = '0.5.28'
   REPO = 'https://github.com/potomak/tomatoes'
   
   class Application < Rails::Application
