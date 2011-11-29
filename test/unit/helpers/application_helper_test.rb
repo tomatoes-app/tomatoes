fc25f4366b7dd8066e26dae3a7232ee84cf6e7c3
Giovanni Cappellotto <potomak84@gmail.com>
2011-11-29 17:05:47 +0100

    Add years counter to humanize helper

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -22,7 +22,10 @@ class ApplicationHelperTest < ActionView::TestCase
   [
     {:secs => 30, :string => '30 seconds'},
     {:secs => 60 + 1, :string => '1 minute, 1 second'},
-    {:secs => 60*2 + 1, :string => '2 minutes, 1 second'}
+    {:secs => 60*2 + 1, :string => '2 minutes, 1 second'},
+    {:secs => 60*60*2 + 60*2 + 1, :string => '2 hours, 2 minutes, 1 second'},
+    {:secs => 24*60*60*2 + 60*60*2 + 60*2 + 1, :string => '2 days, 2 hours, 2 minutes, 1 second'},
+    {:secs => 365*24*60*60*2 + 24*60*60*2 + 60*60*2 + 60*2 + 1, :string => '2 years, 2 days, 2 hours, 2 minutes, 1 second'}
   ].each do |data|
     test "humanize #{data[:secs]} secs returns '#{data[:string]}'" do
       assert humanize(data[:secs]) == data[:string]
