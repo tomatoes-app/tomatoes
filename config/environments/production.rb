205f0a6822938b31da9e08ebcabdba68cbe37aea
Giovanni Cappellotto <potomak84@gmail.com>
2011-12-01 11:50:59 +0100

    Add assets to precompile list array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ TomatoesApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery.flot.js jquery.flot.selection.js user_charts.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
