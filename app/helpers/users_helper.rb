4d58eacafbd7ec5bf036b2b53cb0aba00281ba15
Giovanni Cappellotto <potomak84@gmail.com>
2012-06-14 02:39:24 +0200

    Fix "ActionView::Template::Error ( isn't precompiled)"

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def profile_image(user, size=24)
-    image_tag user.authorizations && !user.authorizations.empty? ? user.authorizations.first.image : '', :width => size
+    image_tag user.authorizations && !user.authorizations.empty? ? user.authorizations.first.image : 'pomodoro-technique.png', :width => size
   end
   
   def user_name(user)
