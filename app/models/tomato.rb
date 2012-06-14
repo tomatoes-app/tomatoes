c8ebe20fc5b558b9b9eca4b43e7b20d5c3e5fd71
Giovanni Cappellotto <potomak84@gmail.com>
2012-06-14 13:46:52 +0200

    Add time limit before next tomato in must_not_overlap error text

--- app/models/tomato.rb
+++ app/models/tomato.rb
@@ -12,9 +12,13 @@ class Tomato
   DURATION       = Rails.env.development? ? 10 : 25*60 # pomodoro default duration in seconds
   BREAK_DURATION = Rails.env.development? ? 5  : 5*60  # pomodoro default break duration in seconds
   
+  include ActionView::Helpers::TextHelper
+  include ApplicationHelper
+
   def must_not_overlap
-    if user.tomatoes.where(:created_at => {'$gte' => Time.zone.now - DURATION.seconds}).first
-      errors.add(:base, "Must not overlap saved tomaotes")
+    if last_tomato = user.tomatoes.where(:created_at => {'$gte' => Time.zone.now - DURATION.seconds}).order_by([[:created_at, :desc]]).first
+      limit = (DURATION.seconds - (Time.zone.now - last_tomato.created_at)).seconds
+      errors.add(:base, "Must not overlap saved tomaotes, please wait #{humanize(limit)}")
     end
   end
 
