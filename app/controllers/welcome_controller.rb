361f679293fdd12e7f8a276442f6ae6374b1e29b
Giovanni Cappellotto <potomak84@gmail.com>
2012-06-27 11:10:05 +0200

    Minor changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      @tomato = current_user.tomatoes.build
+      @tomato   = current_user.tomatoes.build
       @tomatoes = current_user.tomatoes_after(Time.zone.now.beginning_of_day)
     end
   end
