d42394fe8a6bc8d3e6e6cae892eb6058156bbafb
Giovanni Cappellotto <potomak84@gmail.com>
2012-06-27 18:13:12 +0200

    bump version number to 0.6.33

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ if defined?(Bundler)
 end
 
 module TomatoesApp
-  VERSION = '0.6.32'
+  VERSION = '0.6.33'
   REPO = 'https://github.com/potomak/tomatoes'
   
   class Application < Rails::Application
