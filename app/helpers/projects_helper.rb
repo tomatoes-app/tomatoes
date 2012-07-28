f940f15b9a7b7ebb8d3e0e935e49d049e24a45a0
Giovanni Cappellotto <potomak84@gmail.com>
2012-07-28 09:54:12 +0200

    Rename budget to money_budget; add dynamic attributes
    Project's estimated_work_time is the real work time needed to get the
    project done. time_budget attribute is multiplied by WORK_TIME_FACTOR.
    WORK_TIME_FACTOR is a multiplier to convert tomatoes work time to real
    work time.
    
    See #47

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,5 @@
 module ProjectsHelper
+  def money_budget(project)
+    number_to_currency(project.money_budget, unit: User::CURRENCIES[project.user.currency])
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,11 +5,24 @@ class Project
   include Mongoid::Document::Taggable
   include Mongoid::Timestamps
 
-  field :name,        :type => String
-  field :budget,      :type => Integer
-  field :time_budget, :type => Integer
+  TOMATO_TIME_FACTOR   = (Tomato::DURATION*4 + Tomato::BREAK_DURATION*(3+3))/(Tomato::DURATION*4).to_f
+  HOURS_PER_DAY_FACTOR = 24/8.0
+  DAYS_PER_WEEK_FACTOR = 7/5.0
+  WORK_TIME_FACTOR     = DAYS_PER_WEEK_FACTOR * HOURS_PER_DAY_FACTOR * TOMATO_TIME_FACTOR
+
+  field :name,         :type => String
+  field :money_budget, :type => Integer
+  field :time_budget,  :type => Integer
 
   belongs_to :user
 
   validates_presence_of :name
+
+  def estimated_work_time
+    time_budget * WORK_TIME_FACTOR if time_budget
+  end
+
+  def estimated_hourly_rate
+    money_budget / estimated_work_time*60*60 if money_budget && estimated_work_time
+  end
 end
