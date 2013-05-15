8e78f363432d3247813a63fd16f062dac492bf25
Giovanni Cappellotto <potomak84@gmail.com>
2013-05-15 17:58:07 +0200

    Fix resource loading failure
    If FOG_DIRECTORY (the AWS S3 bucket name) contains dots "." they are
    interpreted as subdomains making the certificate for *.s3.amazonaws.com
    useless.
    
    Note: this solution uses link to resources in the form
    s3.amazonaws.com/<bucket>, it works only for US standard zone buckets.
    
    See #88

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ TomatoesApp::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( users.js users_charts.js statistics.js TT.js )
