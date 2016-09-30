746b8686ef23341d808857a21cd17c326a17c0ba
Giovanni Cappellotto <potomak84@gmail.com>
2016-09-29 23:12:17 -0400

    Fix 'interpreted as grouped expression' warning
    Fixes #153.

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
-workers (ENV['WEB_CONCURRENCY'] || 2).to_i
-min_threads_count = max_threads_count = (ENV['RAILS_MAX_THREADS'] || 5).to_i
+workers ENV['WEB_CONCURRENCY'] || 2
+min_threads_count = max_threads_count = ENV['RAILS_MAX_THREADS'] || 5
 threads min_threads_count, max_threads_count
 
 preload_app!
 
 rackup DefaultRackup
-port (ENV['PORT'] || 3000).to_i
+port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
