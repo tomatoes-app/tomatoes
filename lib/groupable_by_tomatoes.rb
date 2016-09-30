96f70b471441564a59b08f35e7df86d40b5b7f31
Giovanni Cappellotto <potomak84@gmail.com>
2016-09-29 23:55:43 -0400

    Avoid multi-line chains of blocks
    Moreover
    
        sort { |a, b| a[0] <=> b[0] }
    
    should be equivalent to
    
        sort_by(&:first)
    
    and
    
        map { |a| [a[0], a[1]] }
    
    just doesn't make any sense in this case.

--- lib/groupable_by_tomatoes.rb
+++ lib/groupable_by_tomatoes.rb
@@ -7,7 +7,7 @@ module GroupableByTomatoes
     def group_by_tomatoes(collection)
       Hash[collection.group_by do |item|
         item.tomatoes.count
-      end.sort { |a, b| a[0] <=> b[0] }.map { |a| [a[0], a[1]] }]
+      end.sort_by(&:first)]
     end
   end
 end
