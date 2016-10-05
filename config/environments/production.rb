03c98c0929c5bc3eb3dd8cf9346c55b36b45f193
Giovanni Cappellotto <potomak84@gmail.com>
2016-10-05 00:43:04 -0400

    Comment out active record configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   config.middleware.use ExceptionNotifier,
                         email_prefix: '[TomatoesApp] ',
