103321829409bf6876d21e3e059e09eeb78473b8
Matteo Centenaro <bugant@gmail.com>
2016-10-12 21:02:52 +0200

    NEW: API login via Twitter
    Authenticate an API user via Twitter access token and secret.
    
    Fix #186

--- app/models/api/auth_factory.rb
+++ app/models/api/auth_factory.rb
@@ -5,8 +5,7 @@ module Api
       when 'github'
         GithubAuth.new(params[:access_token])
       when 'twitter'
-        # TODO: support twitter provider
-        raise StandardError.new('TODO: support twitter provider')
+        TwitterAuth.new(params[:access_token], params[:secret])
       else
         raise Error::ProviderNotSupported.new("Provider '#{params[:provider]}' not supported")
       end
