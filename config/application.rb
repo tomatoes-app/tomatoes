4f1ddff5ee05837def2025cc7a21babb9234cf79
Giovanni Cappellotto <potomak84@gmail.com>
2016-10-15 01:00:23 -0400

    Bump version number

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module TomatoesApp
-  VERSION = '0.8.0'.freeze
+  VERSION = '0.9.0'.freeze
   REPO = 'https://github.com/potomak/tomatoes'.freeze
 
   class Application < Rails::Application
