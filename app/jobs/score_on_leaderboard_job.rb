5392f7f8ffd46a5ddf4d6cfe5ff2a2b54d5259ac
Matteo Centenaro <bugant@gmail.com>
2016-11-03 14:37:46 +0100

    Use time format string to compare time-leaderboard elements and implements all leaderboard types

--- app/jobs/score_on_leaderboard_job.rb
+++ app/jobs/score_on_leaderboard_job.rb
@@ -2,12 +2,15 @@ class ScoreOnLeaderboardJob
   include SuckerPunch::Job
 
   def perform(user_id, score=1)
-    rank_on_leaderboard(user_id, score, UserRankingToday, 'to_date')
+    rank_on_leaderboard(user_id, score, UserRankingToday, '%Y %j')
+    rank_on_leaderboard(user_id, score, UserRankingThisWeek, '%Y %W')
+    rank_on_leaderboard(user_id, score, UserRankingThisMonth, '%Y %m')
+    rank_on_leaderboard(user_id, score, UserRankingAllTime, 'alltime')
   end
 
-  def rank_on_leaderboard(user_id, score, leaderboard_klass, time_method)
+  def rank_on_leaderboard(user_id, score, leaderboard_klass, time_format)
     rank = leaderboard_klass.where(_id: user_id).first
-    if rank && rank.created_at.try(time_method) != Time.now.try(time_method)
+    if time_format != 'alltime' && rank && rank.created_at.strftime(time_format) != Time.now.strftime(time_format)
       SuckerPunch.logger.info("deleting old #{leaderboard_klass.name} ranking for user #{user_id}")
       rank.destroy
       rank = nil
