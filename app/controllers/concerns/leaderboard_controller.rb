7680df3920b879829c8ec78909610c1ff56940a5
Giovanni Cappellotto <potomak84@gmail.com>
2017-01-03 16:54:02 -0500

    Find leaderboard scores in a before filter
    This way the action implementation is free from any detail relative to
    how we retrieve leaderboard scores. The HTML version won't include
    anything, the API version will override the default implementation
    without the need to call `super` to find the scores list.

--- app/controllers/concerns/leaderboard_controller.rb
+++ app/controllers/concerns/leaderboard_controller.rb
@@ -1,5 +1,14 @@
 module LeaderboardController
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :find_scores, only: :show
+  end
+
   def show
+  end
+
+  def find_scores
     @leaderboard = collection.includes(:user).desc(:score).page(params[:page])
   end
 end
