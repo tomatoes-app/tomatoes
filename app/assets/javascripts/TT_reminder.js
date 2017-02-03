016bcb59c5a8c2c475a3ea0673cd279060453178
Giovanni Cappellotto <potomak84@gmail.com>
2017-02-03 00:08:16 -0500

    Cancel reminder on timer stop
    I forgot that the reminder should be cancelled if the timer is started
    again. :P

--- app/assets/javascripts/TT_reminder.js
+++ app/assets/javascripts/TT_reminder.js
@@ -1,20 +1,27 @@
 var TTReminder = function() {
   var reminderDelay = 20; // constant delay in seconds
+  var reminderId;
 
   var delayReminder = function(message) {
-    setTimeout(function() {
+    reminderId = setTimeout(function() {
       // reminder notification
       NOTIFIER.notify(tomatoNotificationIcon, 'Tomatoes', message, true);
     }, reminderDelay * 1000);
   };
 
+  var cancelReminder = function() {
+    clearTimeout(reminderId);
+  };
+
   return {
-    delayReminder: delayReminder
+    delayReminder: delayReminder,
+    cancelReminder: cancelReminder
   };
 }();
 
 // initialize TTReminder
 $(document).ready(function() {
+  $(document).on('timer_start', TTReminder.cancelReminder);
   $(document).on('timer_stop', function() {
     TTReminder.delayReminder('Did you forget to start your next tomato?');
   });
