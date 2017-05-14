8bc7727b7482ea6880e16d02a5f08c5821df1367
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2017-05-14 01:32:45 -0300

    Create tomato with duration on the tests

--- test/functional/tomatoes_controller_test.rb
+++ test/functional/tomatoes_controller_test.rb
@@ -9,7 +9,7 @@ class TomatoesControllerTest < ActionController::TestCase
       name: 'name',
       email: 'email@example.com'
     )
-    @tomato = @user.tomatoes.create(tag_list: 'one, two', created_at: Time.zone.now - 1.day)
+    @tomato = @user.tomatoes.create(tag_list: 'one, two', created_at: Time.zone.now - 1.day, duration: 25)
 
     @controller.stubs(:current_user).returns(@user)
   end
